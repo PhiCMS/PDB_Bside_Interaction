a340c2de2f775488d239d088c837cef4da98de9d
= <91248594+PhiCMS@users.noreply.github.com>
2022-09-21 16:16:04 +0200

    Added small method to detect continous sequence in active side

--- Backend_code/Plip_XML_File_Handel.py
+++ Backend_code/Plip_XML_File_Handel.py
@@ -27,3 +27,30 @@ for side in plipxml.bsites:
         buffer.append((res['resnr'], res['aa']))
     buffer.sort()
     active_side[side] = buffer
+
+
+# test for extracting continuous sequence sections
+# here only the first interaction is checked for test reasons
+
+single_as = active_side[list(active_side.keys())[0]]
+test_out = dict()
+counter = 1
+buffer = list()
+
+for i in range(len(single_as)):
+    if i == 0:
+        buffer.append(single_as[i][1])
+        continue
+
+    if single_as[i-1][0] + 1 == single_as[i][0]:
+        buffer.append(single_as[i][1])
+
+        if i == len(single_as)-1:
+            test_out[counter+1] = buffer
+
+    else:
+        test_out[counter] = buffer
+        counter += 1
+        buffer = list()
+        buffer.append(single_as[i][1])
+
